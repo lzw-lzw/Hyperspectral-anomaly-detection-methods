3929400273da8f2f3bd4d2d480c9680ade7537da
lzw-lzw <22210720038@m.fudan.edu.cn>
2022-09-15 13:38:54 +0800

    update

--- LRASR/LRASR.m
+++ LRASR/LRASR.m
@@ -13,7 +13,8 @@ for i=1:100
     for j=1:100
         anomaly(i,j)=norm(reshape(EE(i,j,:),1,102),2);
     end   
-end        
+end  
+      
 anomaly = (anomaly-min(anomaly(:)))/(max(anomaly(:))-min(anomaly(:)));       
 imshow(anomaly);
 colormap(jet);
--- func_hyperImshow.m
+++ func_hyperImshow.m
@@ -1,8 +1,5 @@
 function [rgb] = func_hyperImshow( hsi, RGBbands )
 %% Hyperspectral Image color display
-% Author: Zephyr Hou
-% Time: 2019-12-02
-% Function Usage
 % Input:
 %    hsi—the 3D hyperspectral dataset with the size of rows x cols x bands
 %    RGBbands— the RGB bands to be displayed, with the format [R G B]
@@ -45,25 +42,5 @@ rgb = zeros(size(hsi, 1), size(hsi, 2), 3);
 rgb(:,:,1) = adapthisteq(red);      % Adaptive histogram equalization
 rgb(:,:,2) = adapthisteq(green);
 rgb(:,:,3) = adapthisteq(blue);
-% J=rgb2gray(rgb);
-% K=fft2(J);
-% L=fftshift(K);
-% size(L)
-% max(max(L))
-% L(630:650,1:620)=0;
-% L(630:650,660:1280)=0;
-% M=ifft2(ifftshift(L));
-% h=[0,1,0;1,-4,1;0,1,0];
-% N=conv2(M,h,'same');
-% P=M-N;
-% subplot(151);
 imshow(rgb); axis image;   % 保持图像的显示比例，其中axis为坐标轴的控制函数。
-% subplot(152);
-% imshow(J);
-% subplot(153);
-% imshow(uint8(abs(L)));
-% subplot(154);
-% imshow(M);
-% subplot(155);
-% imshow(P);
 end
